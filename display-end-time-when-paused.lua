1fd3424c77390ecb70f8a588a1c92e5624c65f6f
seigneurfuo <seigneurfuo@protonmail.com>
2024-10-20 12:54:53 +0200

    Add a option to turn off or on current time.

--- display-end-time-when-paused.lua
+++ display-end-time-when-paused.lua
@@ -1,8 +1,12 @@
 -- Auteur: seigneurfuo
--- Version: 0.2.1
+-- Version: 0.2.2
+
+--- <Config> ---
+local config_osd_duration = 5
+local config_show_current_time = false
+--- </Config> ---
 
 local mp = require 'mp'
-local osd_duration = 2
 local already_paused = false
 
 function on_load()
@@ -31,9 +35,15 @@ function show_message()
     local end_time = now + remaining_seconds
     local end_time_string = os.date('%H:%M:%S', end_time)
 
-    local msg = "Ends at: " .. end_time_string .. "\n" .. "Current time: " .. now_time_string
+    local msg = "Ends at: " .. end_time_string 
+
+    if config_show_current_time then
+        msg = msg .. " (Time: " .. now_time_string .. ")"
+    end
+
+    --local msg = "Ends at: " .. end_time_string .. "\n" .. "Current time: " .. now_time_string
     --local msg = "Current time: " .. now_time_string .. "\nEnds at: " .. end_time_string
-    mp.osd_message(msg, osd_duration)
+    mp.osd_message(msg, config_osd_duration)
 end
 
 mp.add_hook('on_load', 50, on_load)
